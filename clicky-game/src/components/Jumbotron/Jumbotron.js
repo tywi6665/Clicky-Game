8742de3275d8e01edbed281805f779fcbff46d80
tywi6665 <tyletcole@gmail.com>
2018-10-03 13:54:09 -0600

    push

--- clicky-game/src/components/Jumbotron/Jumbotron.js
+++ clicky-game/src/components/Jumbotron/Jumbotron.js
@@ -3,10 +3,10 @@ import "./Jumbotron.css";
 
 const Jumbotron = () => (
     <div className="jumbotron">
-        <div className="container">
+        
             <h1 className="display-4">Clicky Game</h1>
-            <p className="lead">It's time to get your click on!! Your task is simple, click one the pictures before, but be sure not click on the same picture twice!</p>
-        </div>
+            <p className="lead">It's time to get your click on!! Your task is simple, click on the pictures below, but be sure not click on the same picture twice!</p>
+        
     </div>
 );
 
